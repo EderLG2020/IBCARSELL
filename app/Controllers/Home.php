720015eef7823efef5c0f030460530040e449360
Anthony <anthony.z.roldan@gmail.com>
2023-01-13 17:10:39 -0500

    grupo2-termiando

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,7 @@ namespace App\Controllers;
 
 use App\Models\Mostrarvehiculo;
 use App\Models\Vehiculo;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Home extends BaseController {
 
@@ -13,14 +14,42 @@ class Home extends BaseController {
 
         $vehiculo=  new Vehiculo();
         $datos['vehiculos']= $vehiculo->orderBy('cat_id','ASC')->findAll();
-        return view('listar',$datos);
+
+        return view('head').
+        view('listar',$datos);
     }
 
     public function vehiculos($id = null){
         // echo "datos de auto" . $id;
         $vehiculo2=  new Mostrarvehiculo();
-        $datos2['vehiculos2']= $vehiculo2->where('id_categoria',$id)->findAll();
-        return view('lista-vehiculos',$datos2);
+        $datos2['vehiculos2']= $vehiculo2->where('cat_id',$id)->findAll();
+        return view('head').
+        view('lista-vehiculos',$datos2);
+    }
+
+    // public function getVehsssiculo($slug = null){
+
+    //     $getVehiculo=  new Mostrarvehiculo();
+    //     $datos3['vehiculo']= $getVehiculo->where('slug_categoria',$slug)->findAll();
+    //     print_r($datos3);
+    //     return view('head').
+    //     view('lista-vehiculos',$datos3);
+    // }
+
+    public function view($slug = null){
+        $model = model(Mostrarvehiculo::class);
+        $data['vehiculo'] = $model->getVehiculo($slug);
+
+        if(empty($data['vehiculo'])){
+            throw new PageNotFoundException('No se encuentra el vehiculo:' .  $slug);
+            
+        }
+
+       // print_r(json_encode($data['vehiculo']['nombre_vehiculo']));
+        $data['titulo'] = $data['vehiculo'][0]['version'];
+    
+        return view('head').
+        view('info-vehiculo',$data);     
     }
 
     
--- app/Models/Mostrarvehiculo.php
+++ app/Models/Mostrarvehiculo.php
@@ -6,5 +6,28 @@ class Mostrarvehiculo extends Model{
    protected $table      = 'vehiculo';
     // Uncomment below if you want add primary key
    protected $primaryKey = 'id_vehiculo';
-   protected $allowedFields = ['costo_vehiculo','anio_vehiculo'];
+   protected $allowedFields = ['cat_id'];
+
+   public function getVehiculo($slug){
+      $db      = \Config\Database::connect();
+      $builder = $db->table('vehiculo'); 
+      $builder->select('*');
+      $builder->join('caracteristica ', 'caracteristica.idCarac = vehiculo.idCarac');
+      $builder->join('marca ', 'marca.idmarca = vehiculo.idmarca');
+      $builder->where('slug_vehiculo', $slug);
+      $query   = $builder->get();
+      //var_dump($query);
+      return $query->getResultArray(); 
+      // return $this-> where(['slug_vehiculo' => $slug]) -> first();
+   }
+
+   // public function getVehiculo2(){
+   //    $db      = \Config\Database::connect();
+   //    $builder = $db->table('news'); 
+   //    $builder->select('title');
+   //    $builder->join('new', 'cat_new = id_new');
+   //    $query   = $builder->get(); 
+
+   //    return $query->getResultArray();
+   // }
 }
