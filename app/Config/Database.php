61a5b24398f49106e2622012573ef17b7850fe4a
AnderMediBas <andermedib@mail.com>
2023-01-23 08:50:59 -0500

    actualizacion

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
         'DSN'      => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => '',
+        'password' => '29141105',
         'database' => 'ibcarsell',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
